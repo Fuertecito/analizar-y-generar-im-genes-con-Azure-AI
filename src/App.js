7d90b6c3189f71ce29dc5c54a098db4456701264
fuertesito <davidtejero2012@gmail.com>
2023-11-04 20:15:52 +0100

    Commit made from VS Code Azure Static Web Apps

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  // Estado para almacenar la URL de la imagen o la solicitud
+  const [inputValue, setInputValue] = useState('');
+  
+  // Función para manejar cambios en el cuadro de texto
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  }
+
+  // Función para activar el análisis de imágenes
+  const handleImageAnalysis = () => {
+    // Aquí puedes agregar la lógica para el análisis de imágenes
+    // Usando la URL almacenada en 'inputValue'
+    // Por ejemplo, puedes enviar una solicitud a un servidor para procesar la imagen.
+  }
+
+  // Función para activar la generación de imágenes
+  const handleImageGeneration = () => {
+    // Aquí puedes agregar la lógica para la generación de imágenes
+    // Usando la solicitud almacenada en 'inputValue'
+    // Por ejemplo, puedes generar una imagen en base a la solicitud.
+  }
+
+  return (
+    <div>
+      <h1>Aplicación de Análisis y Generación de Imágenes</h1>
+      <input
+        type="text"
+        placeholder="Ingrese la URL de la imagen o solicitud"
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      <br />
+      <button onClick={handleImageAnalysis}>Analizar Imagen</button>
+      <button onClick={handleImageGeneration}>Generar Imagen</button>
+    </div>
+  );
 }
 
 export default App;
